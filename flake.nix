47c27cff1ef3f949f54c71c35aaedb319a4c80d7
chluebi <42419603+chluebi@users.noreply.github.com>
2024-04-15 16:35:36 +0200

    basic flake project structure

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+      in
+      {
+        packages = {
+          default = mkPoetryApplication { projectDir = self; };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = [ pkgs.poetry ];
+        };
+      });
+}
