b73847300ef22415e4fa673ee7c27b242aa2aa57
chluebi <42419603+chluebi@users.noreply.github.com>
2024-08-04 16:32:08 +0200

    updating to new poetry2nix

--- flake.nix
+++ flake.nix
@@ -3,16 +3,38 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.poetry2nix = {
+    url = "github:nix-community/poetry2nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = inputs @ { self, nixpkgs, flake-utils, poetry2nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
       in
       {
         packages = {
-          default = mkPoetryApplication { projectDir = self; };
+          default = poetry2nix.mkPoetryApplication { 
+            projectDir = self;
+
+            overrides = poetry2nix.overrides.withDefaults (self: super: {
+
+              testcontainters = super.testcontainters.overridePythonAttrs (old: {
+                buildInputs = (old.buildInputs or []) ++ [ super.hatchling super.hatch-vcs ];
+              });
+
+              docker = super.docker.overridePythonAttrs (old: {
+                buildInputs = (old.buildInputs or []) ++ [ super.hatchling ];
+                nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ super.hatchling super.hatch-vcs ];
+              });
+
+            });
+
+            buildInputs = [ pkgs.python311Packages.hatchling ];
+                       
+          };
         };
 
         devShells.default = pkgs.mkShell {
