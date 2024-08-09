a998b6d29093ca55548983296f1ce08813e98be3
chluebi <42419603+chluebi@users.noreply.github.com>
2024-08-10 00:20:24 +0200

    bot syncing and database connection

--- Dockerfile
+++ Dockerfile
@@ -27,6 +27,11 @@ RUN pip wheel --no-cache-dir --wheel-dir wheels .
 
 FROM python:3.11-slim AS runner
 
+RUN apt-get update && \
+    apt-get install -y libpq5 && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
 COPY --from=builder /app/wheels /wheels
 RUN pip install --no-cache-dir /wheels/* && rm -rf /wheels
 
--- src/bot/main.py
+++ src/bot/main.py
@@ -1,41 +1,74 @@
-from typing import Optional
+from typing import Optional, Any, List
 
 import os
 
+import sqlalchemy
 import discord
-from discord import app_commands
+from discord.ext import commands
+
+from src.database.postgres import PostgresDatabase
+from src.definitions.start_condition import start_condition
 
 
 # initial setup taken from https://github.com/Rapptz/discord.py/blob/master/examples/app_commands/basic.py
 
-MY_GUILD = discord.Object(id=int(os.environ["DEVELOPMENT_GUILD_ID"]))
+DEVELOPMENT_GUILD = discord.Object(id=int(os.environ["DEVELOPMENT_GUILD_ID"]))
 
 
-class MyClient(discord.Client):
-    def __init__(self, *, intents: discord.Intents):
-        super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self)
+url = sqlalchemy.engine.url.URL.create(
+    drivername="postgresql+psycopg2",
+    username=os.environ["POSTGRES_USER"],
+    password=os.environ["POSTGRES_PASSWORD"],
+    host="db",
+    port=5432,
+    database=os.environ["POSTGRES_DB"],
+)
+
+
+engine = sqlalchemy.create_engine(url)
+db = PostgresDatabase(start_condition, engine)
+
+
+class Bot(commands.Bot):
+
+    def __init__(self, initial_extensions: List[str], db: PostgresDatabase) -> None:
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        super().__init__(command_prefix=commands.when_mentioned_or("com "), intents=intents)
+
+        self.initial_extensions = initial_extensions
+        self.db = db
 
     async def setup_hook(self) -> None:
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
+        for extension in self.initial_extensions:
+            await self.load_extension(extension)
+
+        self.tree.clear_commands(guild=DEVELOPMENT_GUILD)
+        self.tree.copy_global_to(guild=DEVELOPMENT_GUILD)
+        await self.tree.sync(guild=DEVELOPMENT_GUILD)
+
+    async def on_ready(self) -> None:
+        assert self.user is not None
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print("------")
 
 
-intents = discord.Intents.default()
-client = MyClient(intents=intents)
+bot = Bot([], db)
 
 
-@client.event
-async def on_ready() -> None:
-    assert client.user is not None
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
-    print('------')
-
-
-@client.tree.command()
+@bot.tree.command()
 async def hello(interaction: discord.Interaction) -> None:
     """Says hello!"""
-    await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+    await interaction.response.send_message(f"Hi, {interaction.user.mention}")
 
 
-client.run(os.environ["DISCORD_TOKEN"])
+@bot.tree.command()
+@commands.is_owner()
+async def sync(interaction: discord.Interaction) -> None:
+    """Sync commands"""
+    synced = await bot.tree.sync()
+    await interaction.response.send_message(f"Synced {len(synced)} commands globally")
+
+
+bot.run(os.environ["DISCORD_TOKEN"])
